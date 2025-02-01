ac9c95ffd6cf99377073fa4a4243b674959d134a
Ashton Linux <ironslamhead@gmail.com>
2025-01-31 17:43:12 -0800

    Created landingPage, and App now displays it.

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,10 @@
+
+// LandingPage for website
+// Contains background image and welcome message
+export function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page!</h1>
+    </div>
+  );
+}
