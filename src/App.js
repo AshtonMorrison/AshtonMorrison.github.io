c0e90ce42dc3b176171ba508cd204cdd84a937c3
Ashton Linux <ironslamhead@gmail.com>
2025-02-04 12:18:15 -0800

    Added routing from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import LandingPage from "./Pages/LandingPage";
 import AboutMe from "./Pages/AboutMe";
 import Projects from "./Pages/Projects";
+import ContactMe from "./Pages/ContactMe";
 import Navbar from "./Components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 import "./styles.css";
 
@@ -9,9 +11,17 @@ import "./styles.css";
 function App() {
   return (
     <>
-      <LandingPage />
       <Navbar />
-    </> )
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/about' element={<AboutMe />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<ContactMe />} />
+        </Routes>
+      </div>
+    </>
+    )
 }
 
 export default App;
