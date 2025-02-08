5cf19550f519d797a6e1e528b985e488eaa814cd
Ashton Linux <ironslamhead@gmail.com>
2025-02-07 19:18:21 -0800

    Finished draft formatting for About Me page.

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,10 +1,47 @@
 
 export default function AboutMe(){
     return (
-        <div>
-            <h1>
-                About Me Page
+        <div className="about-container">
+            <h1 className="about-header">
+                Who am I?
             </h1>
+            <p className="about-paragraph">
+                Place Holder. Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+            </p>
+
+            <div className="about-section">
+                <img className="about-image-stump" src="/Photos/AshtonStump.jpg" alt="Ashton on a stump looking at camera" />
+
+                <div className="about-section-text">
+                    <h1 className="about-header"> 
+                        TESTING
+                    </h1>
+
+                    <p className="about-paragraph">
+                        Place Holder. Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                        Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                        Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                        Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                    </p>
+                </div>
+            </div>
+            
+            <h1 className="about-header"> 
+                TESTING
+            </h1>
+
+            <img className="about-image-beatles" src="/Photos/AshtonXBeatles.jpg" alt="Ashton walking with friends like the beatles" />
+
+            <p className="about-paragraph">
+                Place Holder. Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+                Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.Place Holder.
+            </p>
+            
         </div>
     )
 }
