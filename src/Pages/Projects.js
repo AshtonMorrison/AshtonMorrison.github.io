3e15c3674b8b8390d24280c41eff8b41803e92b9
Ashton Linux <ironslamhead@gmail.com>
2025-02-09 11:08:51 -0800

    Finished project page formatting, waiting on picture from finished webiste.

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -4,32 +4,31 @@ import {ProjectSectionLeft, ProjectSectionRight} from '../Components/ProjectSect
 export default function Projects(){
     return (
         <div className="container">
-            <h1 className="header">My Projects</h1>
             <ProjectSectionLeft 
-                image="" 
-                title="2d Dungeon Crawler" 
-                text="Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. " 
+                image="/Photos/2DDungeonCrawler.png" 
+                title="2D Dungeon Crawler" 
+                text="This is an ongoing project that I'm working on with two other friends. It's a 2D dungeon crawler game 
+                that's being made in C# using the Monogame framework. We're currently in the very early stages of development, but I believe
+                it's still worth noting. It's so far been a very fun project and a great introduction to game development." 
                 link="https://github.com/YKPandit/Hackathon-Project" 
             />
             <ProjectSectionRight 
-                image="" 
-                title="Java Image Editing Software" 
-                text="Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. " 
+                image="/Photos/Ashton1Mod.png" 
+                title="Image Editing Software" 
+                text="This is an old project that I created for my capstone in high school. It's an image editing program made with Java
+                that allows you to apply various different effects to a given image. It's not the most advanced program in the world, 
+                but it was a great learning experience. The image on the left here was edited using the software, so it gives you an idea
+                of it's capabilities." 
                 link="https://github.com/AshtonMorrison/Image-Editing-Software" 
             />
+
+            {/* Add image of personal site once finished. Inception moment*/}
             <ProjectSectionLeft 
-                image="" 
+                image=""
                 title="Personal Website" 
-                text="Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
-                Place Holder. Place Holder. Place Holder. Place Holder. " 
+                text="Obviously, the website that you're currently on is made by me! This website was made using React, and it was my
+                first time ever doing web development, and also my first time interacting with HTML and CSS. I'm happy with how it turned out,
+                and I'm planning to expand on it throughout my life as I accumulate more and better projects."
                 link="https://github.com/AshtonMorrison/AshtonMorrison.github.io" 
             />
         </div>
