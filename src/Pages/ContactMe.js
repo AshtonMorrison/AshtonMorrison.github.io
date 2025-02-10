08ec6931c777447e08fb01f1e5e3fba2aa056ba6
Ashton Linux <ironslamhead@gmail.com>
2025-02-10 11:20:18 -0800

    Added Place holder text, links, and contact form to ContactMe page

--- src/Pages/ContactMe.js
+++ src/Pages/ContactMe.js
@@ -1,10 +1,21 @@
 
-export default function AboutMe(){
+import Resume from '../Resume.pdf'
+import ContactForm from '../Components/ContactForm.js'
+
+export default function ContactMe(){
     return (
-        <div>
-            <h1>
-                Contact me Page
-            </h1>
+        <div className="container">
+            <p className = 'paragraph'>Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. 
+                Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. Place Holder. </p>
+
+                <div className="section links">
+                    <a className="contact-link" href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
+
+                    <a className="contact-link" href={"https://github.com/AshtonMorrison"} target="_blank" rel="noopener noreferrer">GitHub</a>
+                </div>
+
+                <ContactForm />
         </div>
+
     )
 }
