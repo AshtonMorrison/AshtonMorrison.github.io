ba1d9a27513940fc33910e5ec28323b81f253497
Ashton Linux <ironslamhead@gmail.com>
2025-02-10 11:19:49 -0800

    Created Contact Form and roughly formatted it

---
+++ src/Components/ContactForm.js
@@ -0,0 +1,49 @@
+
+
+export default function ContactForm(){
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "f826bd83-58ff-40c3-ae68-368e638085ed");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          console.log("Success", res);
+        }
+      };
+
+
+    return (
+        <section className="contact-form">
+            <form onSubmit={onSubmit}>
+                <h2>Contact Form</h2>
+                <div className='input-box'>
+                    <label> *Name: </label>
+                    <input type='text' name = "name" className="field" placeholder = "Enter Your Full Name" required />
+                </div>
+                <div className='input-box'>
+                    <label> *Email Address: </label>
+                    <input type='email' name="email" className="field" placeholder = "Enter Your Email" required />
+                </div>
+                <div className='input-box'>
+                    <label> *Message: </label>
+                    <textarea name='message' className="field message" placeholder = "Enter Your Message" required></textarea>
+                </div>
+                <button type='submit' className='submit-btn'>Submit Message</button>
+                <button type='reset' className='reset-btn'>Reset Form</button>
+            </form>
+        </section>
+    )
+}
