796a6da943fae924962e31f6b3c2412a11be71ac
Ashton Linux <ironslamhead@gmail.com>
2026-01-07 14:24:39 -0800

    Added Love At First Paw to project section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -4,20 +4,12 @@ import {ProjectSectionLeft, ProjectSectionRight} from '../Components/ProjectSect
 export default function Projects(){
     return (
         <div className="container">
-            <ProjectSectionLeft 
-                image={import.meta.env.BASE_URL + 'Photos/2DDungeonCrawler.png'} 
-                title="2D Dungeon Crawler" 
-                text="This is an ongoing project that I'm working on with two other friends. It's a 2D dungeon crawler game 
-                that's being made in C# using the Monogame framework. We're currently in the very early stages of development, but I believe
-                it's still worth noting. It's so far been a very fun project and a great introduction to game development." 
-                link="https://github.com/YKPandit/Hackathon-Project" 
-            />
+
             <ProjectSectionRight 
                 image={import.meta.env.BASE_URL + 'Photos/TerritoryAscent.png'} 
                 title="Territory Ascent" 
-                text="This is a simple Local/LAN multiplayer game that I made with 3 classmates for my networking class. 
-                The game allows up to 8 players and is built using Pygame for graphics, Msgpack for data serialization, 
-                and works directly with TCP sockets for communication."
+                text="This is a simple LAN multiplayer game that I made with 3 classmates for my networking class. The game allows up to 8 
+                players and is built using Pygame for graphics, Msgpack for data serialization, and works directly with TCP sockets for communication."
                 link="https://github.com/AshtonMorrison/Territory-Ascent" 
             />
 
@@ -31,6 +23,24 @@ export default function Projects(){
             />
 
             <ProjectSectionRight 
+                image={import.meta.env.BASE_URL + 'Photos/LoveAtFirstPaw.png'} 
+                title="Love At First Paw"
+                text="This is my final project for my Software Engineering class, which I worked on with 4 other classmates as 
+                Project Manager. Its an adoption website that used both PetFinder and MapBox APIs to allow users to find pets available
+                for adoption in their area. The website is now depricated since Petfinder discontinued their public API, but the code is still available"
+                link="https://github.com/CMPT-276-SPRING-2025/final-project-08-oceans"
+            />
+
+            <ProjectSectionLeft
+                image={import.meta.env.BASE_URL + 'Photos/2DDungeonCrawler.png'} 
+                title="2D Dungeon Crawler" 
+                text="This is an ongoing project that I'm working on with two other friends. It's a 2D dungeon crawler game 
+                that's being made in C# using the Monogame framework. We're currently in the very early stages of development, but I believe
+                it's still worth noting. It's so far been a very fun project and a great introduction to game development." 
+                link="https://github.com/YKPandit/Hackathon-Project" 
+            />
+
+            <ProjectSectionRight
                 image={import.meta.env.BASE_URL + 'Photos/Ashton1Mod.png'} 
                 title="Image Editing Software" 
                 text="This is an old project that I created for my capstone in high school. It's an image editing program made with Java
